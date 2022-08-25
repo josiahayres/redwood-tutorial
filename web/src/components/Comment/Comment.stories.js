df4b2dc441aa343df969676347f0e12959a64368
Josiah Ayres <josiah@lend.com.au>
2022-08-25 21:07:57 +1200

    Initial commit

---
+++ web/src/components/Comment/Comment.stories.js
@@ -0,0 +1,41 @@
+import Comment from './Comment'
+
+export const defaultView = () => {
+  return (
+    <div className="m-4">
+      <Comment
+        comment={{
+          id: 1,
+          name: 'Rob Cameron',
+          body: 'This is the first comment!',
+          createdAt: '2020-01-01T12:34:56Z',
+          postId: 1,
+        }}
+      />
+    </div>
+  )
+}
+
+export const moderatorView = () => {
+  mockCurrentUser({
+    roles: 'moderator',
+    id: 1,
+    email: 'moderator@moderator.com',
+  })
+
+  return (
+    <div className="m-4">
+      <Comment
+        comment={{
+          id: 1,
+          name: 'Rob Cameron',
+          body: 'This is the first comment!',
+          createdAt: '2020-01-01T12:34:56Z',
+          postId: 1,
+        }}
+      />
+    </div>
+  )
+}
+
+export default { title: 'Components/Comment' }
